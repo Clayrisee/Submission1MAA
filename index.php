2b4597c5b1d435a0c40653be54a229ea006f67d7
Clayrise <ardikatamah@gmail.com>
2020-01-04 21:36:39 +0700

    second commit xD

--- index.php
+++ index.php
@@ -27,10 +27,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "clayrisedb.database.windows.net";
+    $host = "tcp:clayrisedb.database.windows.net,1433";
     $user = "clayrise";
     $pass = "Sinonnichi19";
-    $db = "dicodingdb (clayrisedb/dicodingdb)";
+    $db = "dicodingdb";
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
         $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
